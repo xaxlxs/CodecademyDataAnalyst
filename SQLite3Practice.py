4913cab548c8797b088c5b15a2c4e3cbea845a6e
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-04-16 18:34:15 +0000

    Recreated TestDB

--- SQLite3Practice.py
+++ SQLite3Practice.py
@@ -2,14 +2,19 @@ import sqlite3
 
 connection = sqlite3.connect('Resources/TestDB.db')
 cursor = connection.cursor()
-# cursor.execute('''CREATE TABLE users (
-#         id INTEGER PRIMARY KEY,
-#         name TEXT,
-#         username TEXT NOT NULL,
-#         created TEXT NOT NULl)''')
-# add_users = [(2, 'Lesley', 'lsly01', '04/01/2022'),(3, 'Nina', 'tiny1', '04/15/2022')]
-# cursor.executemany('''INSERT INTO users VALUES (?,?,?,?)''', add_users)
-# cursor.execute('''INSERT INTO users VALUES (1, 'Carlos', 'los1234', '04/16/2022')''')
-# connection.commit()
 
-cursor.execute("SELECT * FROM users").fetchall()
+cursor.execute('''CREATE TABLE users (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        username TEXT NOT NULL,
+        created TEXT NOT NULl)''')
+
+cursor.execute('''INSERT INTO users VALUES (1, 'Carlos', 'los1234', '04/16/2022')''')
+
+add_users = [(2, 'Lesley', 'lsly01', '04/01/2022'),(3, 'Nina', 'tiny1', '04/15/2022')]
+cursor.executemany('''INSERT INTO users VALUES (?,?,?,?)''', add_users)
+
+connection.commit()
+
+results = cursor.execute("SELECT * FROM users").fetchall()
+print(results)
