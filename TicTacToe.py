52d9b95bc07f921eb885c08472aac52658709553
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-05-18 01:29:00 +0000

    Start TicTacToe

---
+++ TicTacToe.py
@@ -0,0 +1,35 @@
+a = ' '
+b = ' '
+c = ' '
+d = ' '
+e = ' '
+f = ' '
+g = ' '
+h = ' '
+i = ' '
+
+
+board = '  a   b   c ' + '\n1 ' + a + ' | '+ b + ' | ' + c + '\n ---|---|---' + '\n2 ' + d + ' | '+ e + ' | ' + f + '\n ---|---|---' + '\n3 ' + g + ' | '+ h + ' | ' + i
+
+
+print(active_playuer = ': Chose your square.')
+
+
+
+print(board) 
+
+
+'''
+Psudo:
+check which player is active (counter)
+ask player for their move
+if requested square != ' ' ask to pick again (loop)
+increment counter
+set requested square to player's character
+check for winner
+
+
+
+
+
+'''
