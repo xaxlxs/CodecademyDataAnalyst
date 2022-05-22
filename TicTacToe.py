9cb7e668f210fc0e1ee84c7fcd84366344e43fc4
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-05-22 04:24:14 +0000

    Update TicTacToe

--- TicTacToe.py
+++ TicTacToe.py
@@ -13,6 +13,8 @@ dic = {'a1':a1, 'b1':b1, 'c1':c1, 'a2':a2, 'b2':b2, 'c2':c2, 'a3':a3, 'b3':b3, '
 board = '  a   b   c ' + '\n1 ' + a1 + ' | '+ b1 + ' | ' + c1 + '\n ---|---|---' + '\n2 ' + a2 + ' | '+ b2 + ' | ' + c2 + '\n ---|---|---' + '\n3 ' + a3 + ' | '+ b3 + ' | ' + c3
 print(board) 
 
+moves = []
+
 round = 1
 while round < 10:
     if round % 2 == 0:
@@ -21,11 +23,11 @@ while round < 10:
         active_player = "X"
     print("Player " + active_player + " chose your cell.")
     cell = input()
-    space = dic[cell]
-    if space != " ":
+    if cell in moves:
         print('Chose another cell.')
         continue
     else:
+        space = dic[cell]
         space = active_player
         if active_player in (a1 and b1 and c1):
             board = '  a   b   c ' + '\n1 ' + a1 + ' | '+ b1 + ' | ' + c1 + '\n ---|---|---' + '\n2 ' + a2 + ' | '+ b2 + ' | ' + c2 + '\n ---|---|---' + '\n3 ' + a3 + ' | '+ b3 + ' | ' + c3
