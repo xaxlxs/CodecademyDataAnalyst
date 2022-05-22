1437a5c5f2b3ea1dd0a97adf269fa12c734d7d4f
Carlos C <95592181+xaxlxs@users.noreply.github.com>
2022-05-22 17:13:42 +0000

    Update TicTacToe

--- TicTacToe.py
+++ TicTacToe.py
@@ -8,9 +8,7 @@ a3 = ' '
 b3 = ' '
 c3 = ' '
 
-dic = {'a1':a1, 'b1':b1, 'c1':c1, 'a2':a2, 'b2':b2, 'c2':c2, 'a3':a3, 'b3':b3, 'c3':c3}
-
-board = '  a   b   c ' + '\n1 ' + a1 + ' | '+ b1 + ' | ' + c1 + '\n ---|---|---' + '\n2 ' + a2 + ' | '+ b2 + ' | ' + c2 + '\n ---|---|---' + '\n3 ' + a3 + ' | '+ b3 + ' | ' + c3
+board = '\n  a   b   c ' + '\n1 ' + a1 + ' | '+ b1 + ' | ' + c1 + '\n ---|---|---' + '\n2 ' + a2 + ' | '+ b2 + ' | ' + c2 + '\n ---|---|---' + '\n3 ' + a3 + ' | '+ b3 + ' | ' + c3 + '\n'
 print(board) 
 
 moves = []
@@ -21,35 +19,27 @@ while round < 10:
         active_player = "O"
     else:
         active_player = "X"
-    print("Player " + active_player + " chose your cell.")
-    cell = input()
+    cell = input("Player " + active_player + " chose your cell: ")
     if cell in moves:
-        print('Chose another cell.')
+        print('\nCell is already taken. Chose another cell.\n')
+        continue
+    if cell not in ['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']:
+        print('\nInvalid cell. Chose another cell.\n')
         continue
     else:
-        space = dic[cell]
-        space = active_player
-        if active_player in (a1 and b1 and c1):
-            board = '  a   b   c ' + '\n1 ' + a1 + ' | '+ b1 + ' | ' + c1 + '\n ---|---|---' + '\n2 ' + a2 + ' | '+ b2 + ' | ' + c2 + '\n ---|---|---' + '\n3 ' + a3 + ' | '+ b3 + ' | ' + c3
+        globals()[cell] = active_player
+        
+        # Game is declaring a winner if any move is in the below cells.
+
+        if active_player in (a1 and b1 and c1) or active_player in (a2 and b2 and c2) or active_player in (a3 and b3 and c3) or active_player in (a1 and a2 and a3) or active_player in (b1 and b2 and b3) or active_player in (c1 and c2 and c3) or active_player in (a1 and b2 and c3) or active_player in (a3 and b2 and c1):
+            board = '\n  a   b   c ' + '\n1 ' + a1 + ' | '+ b1 + ' | ' + c1 + '\n ---|---|---' + '\n2 ' + a2 + ' | '+ b2 + ' | ' + c2 + '\n ---|---|---' + '\n3 ' + a3 + ' | '+ b3 + ' | ' + c3 + '\n'
             print(board) 
             print("Player " + active_player + " wins!")
             break
         else:
-            board = '  a   b   c ' + '\n1 ' + a1 + ' | '+ b1 + ' | ' + c1 + '\n ---|---|---' + '\n2 ' + a2 + ' | '+ b2 + ' | ' + c2 + '\n ---|---|---' + '\n3 ' + a3 + ' | '+ b3 + ' | ' + c3
+            board = '\n  a   b   c ' + '\n1 ' + a1 + ' | '+ b1 + ' | ' + c1 + '\n ---|---|---' + '\n2 ' + a2 + ' | '+ b2 + ' | ' + c2 + '\n ---|---|---' + '\n3 ' + a3 + ' | '+ b3 + ' | ' + c3 + '\n'
             print(board)
+            round += 1
+            moves.append(cell)
             continue
-        round += 1
 
-
-
-
-
-'''
-Psudo:
-check which player is active (counter)
-ask player for their move
-if requested square != ' ' ask to pick again (loop)
-increment counter
-set requested square to player's character
-check for winner
-'''
